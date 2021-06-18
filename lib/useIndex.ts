34f99dd34cff1c79a27cf42d21aa1db600c31844
François Chalifour <francois.chalifour@gmail.com>
2021-06-18 12:15:45 +0200

    Get parent index

--- lib/useIndex.ts
+++ lib/useIndex.ts
@@ -1,21 +1,18 @@
 import index from 'instantsearch.js/cjs/widgets/index/index';
 import { useEffect, useMemo } from 'react';
 import { useIndexContext } from './useIndexContext';
-import { useInstantSearchContext } from './useInstantSearchContext';
 
 export function useIndex(props: any) {
   const searchIndex = useIndexContext();
-  const search = useInstantSearchContext();
   const widget = useMemo(() => index(props), [props]);
-  const parentIndex = searchIndex ?? search;
 
   useEffect(() => {
-    parentIndex.addWidgets([widget]);
+    searchIndex.addWidgets([widget]);
 
     return () => {
-      parentIndex.removeWidgets([widget]);
+      searchIndex.removeWidgets([widget]);
     };
-  }, [parentIndex, props, widget]);
+  }, [searchIndex, widget]);
 
   return widget;
 }
