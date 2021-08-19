db1178d62eeb6133dc78040685aafcf488f8856f
François Chalifour <francoischalifour@users.noreply.github.com>
2021-08-19 18:40:57 +0200

    Support control mode and refresh

--- lib/useInstantSearch.ts
+++ lib/useInstantSearch.ts
@@ -1,10 +1,31 @@
-import instantsearch, { InstantSearchOptions } from 'instantsearch.js';
-import { useEffect, useMemo } from 'react';
+import { version as ReactVersion } from 'react';
+import { dequal } from 'dequal/lite';
+import instantsearch, { InstantSearchOptions, UiState } from 'instantsearch.js';
+import { useEffect, useMemo, useRef } from 'react';
 
-type UseInstantSearchProps = InstantSearchOptions;
+import { version } from './version';
+import { useStableValue } from './useStableValue';
 
-export function useInstantSearch(props: UseInstantSearchProps) {
-  const search = useMemo(() => instantsearch(props), [props]);
+export type UseInstantSearchProps = InstantSearchOptions & {
+  refresh?: boolean;
+  uiState?: UiState;
+};
+
+export function useInstantSearch({
+  refresh,
+  uiState,
+  ...props
+}: UseInstantSearchProps) {
+  const stableProps = useStableValue(props);
+  const stableUiState = useStableValue(uiState);
+  const search = useMemo(() => instantsearch(stableProps), [stableProps]);
+
+  useEffect(() => {
+    if (typeof props.searchClient.addAlgoliaAgent === 'function') {
+      props.searchClient.addAlgoliaAgent(`react (${ReactVersion})`);
+      props.searchClient.addAlgoliaAgent(`react-instantsearch (${version})`);
+    }
+  }, [props.searchClient]);
 
   useEffect(() => {
     search.start();
@@ -13,6 +34,22 @@ export function useInstantSearch(props: UseInstantSearchProps) {
       search.dispose();
     };
   }, [search]);
+
+  useEffect(() => {
+    if (!stableUiState || Object.keys(stableUiState).length === 0) {
+      return;
+    }
+
+    search.setUiState(stableUiState);
+  }, [stableUiState, search]);
+
+  useEffect(() => {
+    if (!refresh) {
+      return;
+    }
+
+    search.refresh();
+  }, [refresh, search]);
 
   return search;
 }
