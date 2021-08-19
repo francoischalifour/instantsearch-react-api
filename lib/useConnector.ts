da6eaee0c35936464b32ffc1f15883ccfacea768
François Chalifour <francoischalifour@users.noreply.github.com>
2021-08-19 18:40:32 +0200

    Support updating widget via skipping `init`

--- lib/useConnector.ts
+++ lib/useConnector.ts
@@ -1,25 +1,45 @@
+import {
+  Connector,
+  WidgetDescription,
+  WidgetRenderState,
+} from 'instantsearch.js';
 import { useEffect, useState } from 'react';
+import { useStableValue } from './useStableValue';
 import { useIndexContext } from './useIndexContext';
 import { usePanelContext } from './usePanelContext';
 
 export function useConnector<
-  TProps extends Record<string, any>,
-  TState extends Record<string, any>
->(connector: Function, props: TProps, initialState: TState) {
+  TProps extends Record<string, unknown>,
+  TState extends Omit<WidgetRenderState<any, TProps>, 'widgetParams'>,
+  TDescription extends WidgetDescription
+>(
+  connector: Connector<TDescription, TProps>,
+  props: TProps,
+  initialState: TState | (() => TState)
+) {
   const searchIndex = useIndexContext();
   const panel = usePanelContext();
-  const [state, setState] = useState(() => initialState);
+
+  const [state, setState] = useState(initialState);
+  const stableProps = useStableValue(props);
 
   useEffect(() => {
-    const createWidget = connector(setState);
-    const widget = createWidget(props);
+    const createWidget = connector((state, isFirstRender) => {
+      // We skip the `init` widget render to avoid UI flashes.
+      if (isFirstRender) {
+        return;
+      }
+
+      setState(state);
+    });
+    const widget = createWidget(stableProps);
 
     searchIndex.addWidgets([widget]);
 
     return () => {
       searchIndex.removeWidgets([widget]);
     };
-  }, [connector, searchIndex, props]);
+  }, [connector, searchIndex, stableProps]);
 
   useEffect(() => {
     if (panel) {
